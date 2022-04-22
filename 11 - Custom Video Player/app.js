3e6f55c96db8fa7639f3112ffbaf6e62971a8fa3
Alkım Caner Soydan <soydan101a@gmail.com>
2022-04-22 07:32:31 +0300

    Add fulscreen button to custom video player

--- 11 - Custom Video Player/app.js
+++ 11 - Custom Video Player/app.js
@@ -3,6 +3,7 @@ const video = player.querySelector(".viewer");
 const progress = player.querySelector(".progress");
 const progressBar = player.querySelector(".progress__filled");
 const toggle = player.querySelector(".toggle");
+const fullscreen = player.querySelector(".fullscreen");
 const skipButtons = player.querySelectorAll("[data-skip]");
 const ranges = player.querySelectorAll(".player__slider");
 
@@ -35,6 +36,10 @@ function scrub(e) {
   video.currentTime = scrubTime;
 }
 
+function toggleFullscreen() {
+  video.requestFullscreen();
+}
+
 video.addEventListener("click", togglePlay);
 video.addEventListener("play", updateButton);
 video.addEventListener("pause", updateButton);
@@ -42,6 +47,8 @@ video.addEventListener("timeupdate", handleProgress);
 
 toggle.addEventListener("click", togglePlay);
 skipButtons.forEach((button) => button.addEventListener("click", skip));
+fullscreen.addEventListener("click", toggleFullscreen);
+
 ranges.forEach((range) => range.addEventListener("change", handleRangeUpdate));
 ranges.forEach((range) =>
   range.addEventListener("mousemove", handleRangeUpdate)
